a9317b73a25468f6497979ca984908d142774bb7
vkensou <boyplaydota@hotmail.com>
2023-10-07 21:53:01 +0800

    修复一个错误

--- src/backend/vulkan/src/cgpu_vulkan_resources.cpp
+++ src/backend/vulkan/src/cgpu_vulkan_resources.cpp
@@ -798,7 +798,6 @@ CGPUTextureId cgpu_create_texture_vulkan(CGPUDeviceId device, const struct CGPUT
         {
             imageCreateInfo.flags |= VK_IMAGE_CREATE_SPARSE_BINDING_BIT;
             imageCreateInfo.flags |= VK_IMAGE_CREATE_SPARSE_RESIDENCY_BIT;
-            imageCreateInfo.tiling = VK_IMAGE_TILING_LINEAR;
         }
 
         // Verify that GPU supports this format
