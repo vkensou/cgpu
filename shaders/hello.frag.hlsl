4d100c43e780e2a47784d957126d378a8157a777
vkensou <boyplaydota@hotmail.com>
2024-01-16 16:44:16 +0800

    shader都改为hlsl格式

---
+++ shaders/hello.frag.hlsl
@@ -0,0 +1,9 @@
+struct VSOutput
+{
+[[vk::location(0)]] float3 Color : COLOR0;
+};
+
+float4 main(VSOutput input) : SV_TARGET
+{
+	return float4(input.Color, 1);
+}
---
+++ shaders/hello.vert.hlsl
@@ -0,0 +1,30 @@
+static const float4 positions[3] = {
+    float4( 0.0,  0.5, 0, 1),
+    float4(-0.5, -0.5, 0, 1),
+	float4( 0.5, -0.5, 0, 1)
+};
+
+static const float3 colors[3] = {
+	float3(1.0, 0.0, 0.0),
+	float3(0.0, 1.0, 0.0),
+	float3(0.0, 0.0, 1.0),
+};
+
+struct VSInput
+{
+    uint vertexId : SV_VERTEXID;
+};
+
+struct VSOutput
+{
+	float4 Pos : SV_POSITION;
+[[vk::location(0)]] float3 Color : COLOR0;
+};
+
+VSOutput main(VSInput input)
+{
+	VSOutput output = (VSOutput)0;
+	output.Color = colors[input.vertexId];
+	output.Pos = positions[input.vertexId];
+	return output;
+}
