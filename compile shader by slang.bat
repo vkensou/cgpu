3d7ef79beec67721f012e4176a8425baa2d02169
vkensou <boyplaydota@hotmail.com>
2024-01-16 19:45:06 +0800

    slang生成的shader不带_0后缀了

--- compile shader by slang.bat
+++ compile shader by slang.bat
@@ -1,5 +1,5 @@
-.\tools\slang\slangc shaders\hello.hlsl -profile sm_5_0 -entry vert -o shaders\hello.vert.spv -O3
-.\tools\slang\slangc shaders\hello.hlsl -profile sm_5_0 -entry frag -o shaders\hello.frag.spv -O3
+.\tools\slang\slangc shaders\hello.hlsl -profile sm_5_0 -entry vert -o shaders\hello.vert.spv -O3 -emit-spirv-directly
+.\tools\slang\slangc shaders\hello.hlsl -profile sm_5_0 -entry frag -o shaders\hello.frag.spv -O3 -emit-spirv-directly
 
-.\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry vert -o shaders\imgui.vert.spv -O3
-.\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry frag -o shaders\imgui.frag.spv -O3
+.\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry vert -o shaders\imgui.vert.spv -O3 -emit-spirv-directly
+.\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry frag -o shaders\imgui.frag.spv -O3 -emit-spirv-directly
