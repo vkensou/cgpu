508e4328cedd271b4367b7d8d328a7d3da916ee9
vkensou <boyplaydota@hotmail.com>
2024-01-17 00:09:52 +0800

    移动文件目录 实装use_vulkan选项

--- xmake.lua
+++ xmake.lua
@@ -19,8 +19,8 @@ add_requires("imgui v1.89.8-docking", {configs = {debug = true}})
 
 if is_os("windows") or is_os("linux") or is_os("android")  then
     option("use_vulkan")
-        set_default(false)
-        add_defines("CGPU_USE_VULKAN")
+        set_showmenu(true )
+        set_default(true)
 end
 
 target("cgpu")
@@ -29,38 +29,38 @@ target("cgpu")
     add_packages("xxhash")
     add_packages("parallel-hashmap")
     
-    -- add_options("use_vulkan")
+    add_options("use_vulkan")
 
-    add_includedirs("include", {public = true})
-    add_headerfiles("include/cgpu/*.h")
-    add_includedirs("src/common/include", {public = false})
-    add_headerfiles("src/common/include/*.h")
-    add_files("src/common/*.c")
-    add_files("src/common/*.cpp")
+    add_includedirs("src/cgpu/include", {public = true})
+    add_headerfiles("src/cgpu/include/cgpu/*.h")
+    add_includedirs("src/cgpu/common/include", {public = false})
+    add_headerfiles("src/cgpu/common/include/*.h")
+    add_files("src/cgpu/common/*.c")
+    add_files("src/cgpu/common/*.cpp")
 
-    -- if is_config("use_vulkan") then
+    if has_config("use_vulkan") then
         add_defines("CGPU_USE_VULKAN")
         add_defines("VK_NO_PROTOTYPES")
         add_packages("volk")
         add_packages("spirv-reflect")
-        add_includedirs("src/backend/vulkan/include")
-        add_headerfiles("src/backend/vulkan/include/*.h")
-        add_files("src/backend/vulkan/src/cgpu_vulkan.c")
-        add_files("src/backend/vulkan/src/cgpu_vulkan2.c")
-        add_files("src/backend/vulkan/src/proc_table.c")
-        add_files("src/backend/vulkan/src/cgpu_vulkan_surfaces.c")
-        add_files("src/backend/vulkan/src/cgpu_vulkan_instance.cpp")
-        add_files("src/backend/vulkan/src/cgpu_vulkan_resources.c")
-        add_files("src/backend/vulkan/src/vulkan_utils.c")
-        add_files("src/backend/vulkan/src/vma.cpp")
-        add_files("src/backend/vulkan/src/volk.c")
+        add_includedirs("src/cgpu/backend/vulkan/include")
+        add_headerfiles("src/cgpu/backend/vulkan/include/*.h")
+        add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan.c")
+        add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan2.c")
+        add_files("src/cgpu/backend/vulkan/src/proc_table.c")
+        add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan_surfaces.c")
+        add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan_instance.cpp")
+        add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan_resources.c")
+        add_files("src/cgpu/backend/vulkan/src/vulkan_utils.c")
+        add_files("src/cgpu/backend/vulkan/src/vma.cpp")
+        add_files("src/cgpu/backend/vulkan/src/volk.c")
         if is_os("windows") then
             add_defines("VK_USE_PLATFORM_WIN32_KHR")
-            add_files("src/backend/vulkan/src/cgpu_vulkan_windows.c")
+            add_files("src/cgpu/backend/vulkan/src/cgpu_vulkan_windows.c")
         end
-    -- end
+    end
 
-target("arena")
+target("demo")
     set_kind("binary")
     set_rundir("$(projectdir)")
 
@@ -68,4 +68,4 @@ target("arena")
     add_packages("libsdl")
     add_packages("imgui")
 
-    add_files("arena/*.cpp")
+    add_files("src/demo/*.cpp")
