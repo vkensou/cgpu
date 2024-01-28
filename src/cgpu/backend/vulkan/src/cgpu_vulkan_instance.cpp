93e2529b46715513dfaa935922913aa387717c69
vkensou <boyplaydota@hotmail.com>
2024-01-28 16:33:10 +0800

    动态选择vulkan版本

--- src/cgpu/backend/vulkan/src/cgpu_vulkan_instance.cpp
+++ src/cgpu/backend/vulkan/src/cgpu_vulkan_instance.cpp
@@ -209,7 +209,10 @@ CGPUInstanceId cgpu_create_instance_vulkan(CGPUInstanceDescriptor const* desc)
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_1;
+
+    uint32_t apiVersion;
+    vkEnumerateInstanceVersion(&apiVersion);
+    appInfo.apiVersion = apiVersion;
 
     // Select Instance Layers & Layer Extensions
     VkUtil_SelectInstanceLayers(I,
