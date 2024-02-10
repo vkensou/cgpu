00087dc0bb06334664b7f9b1752464880f2e9dc0
vkensou <boyplaydota@hotmail.com>
2024-02-10 19:07:13 +0800

    更新shader编译脚本

--- compile shader by slang dev.bat
+++ compile shader by slang dev.bat
@@ -3,3 +3,6 @@
 
 .\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry vert -o shaders\imgui.vert.spv -O0 -g3 -line-directive-mode none -emit-spirv-directly
 .\tools\slang\slangc shaders\imgui.hlsl -profile sm_5_0 -entry frag -o shaders\imgui.frag.spv -O0 -g3 -line-directive-mode none -emit-spirv-directly
+
+.\tools\slang\slangc shaders\oval.hlsl -profile sm_5_0 -entry vert -o shaders\oval.vert.spv -O0 -g3 -line-directive-mode none -emit-spirv-directly
+.\tools\slang\slangc shaders\oval.hlsl -profile sm_5_0 -entry frag -o shaders\oval.frag.spv -O0 -g3 -line-directive-mode none -emit-spirv-directly
