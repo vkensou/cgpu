ec03e6863d09e79823e086e317f29664681d641e
vkensou <boyplaydota@hotmail.com>
2025-04-16 12:55:54 +0800

    add idl scripts

---
+++ idl/temp.cgpu.h
@@ -0,0 +1,13 @@
+#ifndef CGPU_C99_H_HEADER_GUARD
+#define CGPU_C99_H_HEADER_GUARD
+
+#include <stdarg.h>  // va_list
+#include <stdbool.h> // bool
+#include <stdint.h>  // uint32_t
+#include <stdlib.h>  // size_t
+
+$cenums
+
+$cflags
+
+#endif // CGPU_C99_H_HEADER_GUARD
