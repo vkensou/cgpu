54a91955fc996764b830ddf619b52e9c2003e40a
vkensou <boyplaydota@hotmail.com>
2026-03-08 12:27:07 +0800

    update slang tools

--- compile shader by dxc dev.bat
+++
@@ -1,5 +0,0 @@
-.\tools\dxc\dxc shaders\hello.hlsl -spirv -T vs_6_0 -E vert -fspv-entrypoint-name=main -Fo shaders\hello.vert.spv -Od -Zi
-.\tools\dxc\dxc shaders\hello.hlsl -spirv -T ps_6_0 -E frag -fspv-entrypoint-name=main -Fo shaders\hello.frag.spv -Od -Zi
-
-.\tools\dxc\dxc shaders\imgui.hlsl -spirv -T vs_6_0 -E vert -fspv-entrypoint-name=main -Fo shaders\imgui.vert.spv -Od -Zi
-.\tools\dxc\dxc shaders\imgui.hlsl -spirv -T ps_6_0 -E frag -fspv-entrypoint-name=main -Fo shaders\imgui.frag.spv -Od -Zi
--- compile shader by dxc.bat
+++
@@ -1,5 +0,0 @@
-.\tools\dxc\dxc shaders\hello.hlsl -spirv -T vs_6_0 -E vert -fspv-entrypoint-name=main -Fo shaders\hello.vert.spv -O3
-.\tools\dxc\dxc shaders\hello.hlsl -spirv -T ps_6_0 -E frag -fspv-entrypoint-name=main -Fo shaders\hello.frag.spv -O3
-
-.\tools\dxc\dxc shaders\imgui.hlsl -spirv -T vs_6_0 -E vert -fspv-entrypoint-name=main -Fo shaders\imgui.vert.spv -O3
-.\tools\dxc\dxc shaders\imgui.hlsl -spirv -T ps_6_0 -E frag -fspv-entrypoint-name=main -Fo shaders\imgui.frag.spv -O3
